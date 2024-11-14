585fd820306f84c2d7b4d3ceb85b9e9f1e45476d
Sumit <sumit.shrestha@olivegroup.io>
2024-11-14 13:04:52 +0545

    working all scenes expect laddertop movement

--- Assets/InventorySystem.cs
+++ Assets/InventorySystem.cs
@@ -60,7 +60,7 @@ public class InventorySystem : MonoBehaviour
             ladder.SetActive(true);
             GameObject cameraoffset = GameObject.Find("Camera Offset");
            
-            ladder.transform.position = UiCanvasController.instance.head.position + new Vector3(UiCanvasController.instance.head.forward.x, 0, UiCanvasController.instance.head.forward.z).normalized * 3;
+            ladder.transform.position = UiCanvasController.instance.head.position + new Vector3(UiCanvasController.instance.head.forward.x, 0, UiCanvasController.instance.head.forward.z).normalized * 1;
             ladder.transform.LookAt(new Vector3(UiCanvasController.instance.head.position.x, ladder.transform.position.y, UiCanvasController.instance.head.position.z));
             //  ladder.transform.position = cameraoffset.transform.position + cameraoffset.transform.forward* 2;
             ladder.transform.position = new Vector3(ladder.transform.position.x, 0, ladder.transform.position.z);
--- Assets/LadderTrigger.cs
+++ Assets/LadderTrigger.cs
@@ -1,24 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LadderTrigger : MonoBehaviour
 {
+    public ContinuousMoveProviderBase moveProvider;
 
     public int taskNumber, taskCompletePercentage;
+
+    private CharacterController characterController;
+    public InputActionReference leftJoystickMoveAction;
+
     public void LadderGrabbed()
     {
+        if (taskNumber==0)
+        {
+            return;
+        }
         TaskManagerCount.instance.TaskCompleted(taskNumber, taskCompletePercentage);
     }
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+       moveProvider = FindObjectOfType<ContinuousMoveProviderBase>();
+        characterController = moveProvider.GetComponent<CharacterController>();
+
+    }
+    // Start is called before the first frame update
+    public void DisableMovement()
+    {
+        Debug.Log("Ladder climbing");
+      //  moveProvider.useGravity = false;
+        moveProvider.enabled = false;
+      
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EnableMovement()
     {
-        
+        Debug.Log("Ladder deselected");
+        //   moveProvider.useGravity = true;
+        //  moveProvider.moveSpeed = 1.2f;
+
+        //  moveProvider.enabled = true;
+        StartCoroutine(ResumeMovementWithDelay());
+
+    }
+    private IEnumerator ResumeMovementWithDelay()
+    {
+        yield return new WaitForSeconds(0.01f); // Small delay to let other scripts finish
+        if (moveProvider != null)
+            moveProvider.enabled = true;
     }
 }
